f4a9c4aa2b7a3d33b22c29f0289802618a3a80e9
unknown <alakmodak99@gmail.com>
2022-09-02 01:31:31 +0530

    Auth-G-F

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider, signInWithPopup,FacebookAuthProvider } from "firebase/auth";
 
-/*const firebaseConfig = {
-    apiKey: "AIzaSyALy3NjHS7WIeI1Xr2MkL0zKVYz4XtSvGE",
-    authDomain: "auth-876d2.firebaseapp.com",
-    projectId: "auth-876d2",
-    storageBucket: "auth-876d2.appspot.com",
-    messagingSenderId: "764748868726",
-    appId: "1:764748868726:web:e8c8652a5930c96ed58357"
-  };*/
   const firebaseConfig = {
     apiKey: "AIzaSyAt7ep9iQcGnDwWimtYvi2hUXzHsR6rIFY", 
     authDomain: "auth-bolt.firebaseapp.com", 
@@ -26,11 +18,8 @@ const googleProvider = new GoogleAuthProvider();
 export const signInWithGoogle = async () =>{
     signInWithPopup(auth, googleProvider).then(resp=>{
         console.log(resp)
-    //   var obj = {name:resp.user.displayName, email:resp.user.email, profile:resp.user.photoURL};
-    //   let email = obj.email.replace(/[^a-zA-Z0-9 ]/g, '');
-    //   console.log(email, obj)
 })};
-
+//How to use Regex?
 export const signInWithFacebook = async () =>{
     const provider=new FacebookAuthProvider()
     signInWithPopup(auth,provider)
